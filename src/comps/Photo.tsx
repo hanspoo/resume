907b2b809de9c3a60b1a63e6e12b98b1edef1b83
hans <hans@welinux.cl>
2024-05-12 20:45:31 -0400

    Resume

--- src/comps/Photo.tsx
+++ src/comps/Photo.tsx
@@ -3,7 +3,7 @@ import photo from "./photo.jpg";
 export function Photo() {
   return (
     <div className="pt-6 content-center">
-      <img src={photo} style={{ width: "200px" }} className="content-center" />
+      <img src={photo} style={{ width: "150px" }} className="content-center" />
     </div>
   );
 }
