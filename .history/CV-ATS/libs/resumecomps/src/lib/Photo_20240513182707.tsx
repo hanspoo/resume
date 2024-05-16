027f96ad10010305032d1ae56186f2e6a0ddddae
a-larrondo <al2.lpz@gmail.com>
2024-05-16 01:39:48 -0400

    borrar history

--- .history/CV-ATS/libs/resumecomps/src/lib/Photo_20240513182707.tsx
+++
@@ -1,9 +0,0 @@
-import photo from "./photo.jpg";
-
-export function Photo() {
-  return (
-    <div className="pt-6 content-center">
-      <img src={photo} style={{ width: "150px" }} className="content-center" />
-    </div>
-  );
-}
